c6b547a3c87d21ca6bef5657e988b6017b0b7c17
hwl9118 <hwl9118@gmail.com>
2021-12-26 16:25:46 +0900

    local storage 에 username 저장, 저장 한 이후 저장된 함수값을 불러와 h1 꾸밀 수 있게 만들었다. 중복 코드가 있어 함수로 변경, 코드 줄을 줄이는 작업이 수행되었음.

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreeting(username);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+const savedUsename = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsename === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreeting(savedUsename);
+}
